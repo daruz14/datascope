d2cd5dd54ac2974e6e4b7d78750bbfe812194697
David Ruz <daruz@uc.cl>
2024-01-09 23:11:42 -0300

    Add section to schedule events with colleagues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = current_user
     @events = @user.events.today
     @colleagues = @user.company ? @user.company.users.where.not(id: @user.id) : []
+    @new_event = Event.new
   end
 
   # GET /users/new
