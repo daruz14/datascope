aa320814a0b7bb327594841f22afc0d327a3add6
David Ruz <daruz@uc.cl>
2024-01-10 15:56:37 -0300

    Add use of address in events and show events location weather when event date is current date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,8 @@ class EventsController < ApplicationController
 
   # GET /events/1 or /events/1.json
   def show
+    @colleagues = @event.users
+    @weather = fetch_weather_data
   end
 
   # GET /events/new
@@ -47,7 +49,15 @@ class EventsController < ApplicationController
   # PATCH/PUT /events/1 or /events/1.json
   def update
     respond_to do |format|
+      changes_address = @event.address != event_params[:address]
+
       if @event.update(event_params)
+
+        if !@event.address || changes_address
+          @event.geocode
+          @event.save!
+        end
+
         format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
         format.json { render :show, status: :ok, location: @event }
       else
@@ -75,11 +85,30 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :start_date, :end_date, user_ids: [])
+      params.require(:event).permit(:title, :start_date, :end_date, :address, user_ids: [])
         .tap do |event_params|
         if current_user && !event_params[:user_ids].include?(current_user.id)
           event_params[:user_ids] << current_user.id
         end
       end
+    end
+
+    def fetch_weather_data
+      current_date = Date.current
+
+      if @event.start_date.to_date != current_date || !@event.address
+        return
+      end
+
+      api_key = ENV.fetch('OPENWEATHER_APIKEY', '')
+      response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?lat=#{@event.latitude}&lon=#{@event.longitude}&appid=#{api_key}&units=metric")
+
+      weather_data = JSON.parse(response.body)
+      return {
+        main: weather_data['weather'][0]['main'],
+        min: weather_data['main']['temp_min'],
+        max: weather_data['main']['temp_max'],
+        current: weather_data['main']['temp']
+      }
     end
 end
