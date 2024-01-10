c2725c2d9070b9c67b36b08d819918b8c446a7f9
David Ruz <daruz@uc.cl>
2024-01-09 23:03:21 -0300

    Add new view, controller and route profile to get self current today events and know colleagues

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,6 @@ class Event < ApplicationRecord
   scope :in_same_company_as, ->(user) {
     joins(:users).where(users: { company: user.company }).distinct
   }
+
+  scope :today, -> { where('start_date >= ? AND start_date <= ?', Time.zone.now.beginning_of_day, Time.zone.now.end_of_day).order(start_date: :asc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :users
-  root 'users#index'
+  root 'users#profile'
+
+  get 'profile', to: 'users#profile', as: 'profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
