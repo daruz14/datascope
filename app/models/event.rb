688781fc03e983f72f79bf6411e2289cc94dd081
David Ruz <daruz@uc.cl>
2024-01-09 23:17:09 -0300

    Add validation to disable creation of events with overlapping

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
+  validate :no_overlapping_events, on: :create
+  validate :end_date_after_start_date, on: :create
 
   scope :in_same_company_as, ->(user) {
     joins(:users).where(users: { company: user.company }).distinct
@@ -9,4 +11,22 @@ class Event < ApplicationRecord
   scope :today, -> { where('start_date >= ? AND start_date <= ?', Time.zone.now.beginning_of_day, Time.zone.now.end_of_day).order(start_date: :asc) }
 
   scope :from_today, -> { where('start_date >= ?', Time.zone.now.beginning_of_day).order(start_date: :asc) }
+  
+  private
+
+  def end_date_after_start_date
+    if end_date < start_date
+      errors.add :end_date, "La fecha de fin del evento debe ser después de la de inicio"
+    end
+  end
+
+  def no_overlapping_events
+    if Event.joins(:attendances)
+      .where(attendances: { user_id: user_ids })
+      .where('events.start_date <= ? AND events.end_date >= ?', end_date, start_date)
+      .exists?
+
+      errors.add(:base, 'Existen usuarios con colisión de horarios, intenta con uno con disponibilidad para todos')
+    end
+  end
 end
